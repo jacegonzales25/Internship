17021abfe98df92cb5f538ac71462572ba958f95
Jace Gonzales <jacegonzales25@gmail.com>
2023-06-20 13:16:46 +0800

    Added axios post, and handling File Change

--- bistro_email_sender/app/TGIF/page.tsx
+++ bistro_email_sender/app/TGIF/page.tsx
@@ -2,35 +2,65 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 export default function TGIF() {
+    
     const formData = new FormData();
 
     const [form, setForm] = useState({
         name: "",
         email: "",
-        resume: ""
     });
 
-
-
     function submitForm(event: any) {
-        
+        formData.append('name', form.name);
+        formData.append('email', form.email);
     }
 
     function handleChange(event: any){
-        setForm({
-            ...form,
-            [event.target.name]: event})
+        const {name, value} = event.target
+        setForm(prevState => {
+            return {
+                ...prevState,
+                [name]: value
+            }
+        })
     }
     
+    const [file, setFile] = useState(undefined);
 
-    
+    function handleFileChange(event: any){
+      // Handling 1 resume passed
+      const file = event.target.files[0];
+      setFile(file);
+      formData.append('file', file);
+    }
+
+    axios.post("apply@bistro.com.ph", formData, {
+     headers: {
+       'Content-Type': 'multipart/form-data'
+     }
+   })
+     .then(function (response) {
+       // Handle success
+       console.log(response);
+     })
+     .catch(function (error) {
+       // Handle error
+       console.log(error);
+     });
 
     return (
       <div>
         <form>
             <label>TGIF Sender</label>
-            <input name="name" value={form.name} placeholder="Name"></input>
-            <input name="email" value={form.email} placeholder="Email"></input>
+           
+            <label>Name:</label>
+            <input type="text" name="name" onChange={handleChange} value={form.name} placeholder="Name"></input>
+            
+            <label>Email:</label>
+            <input type="email" name="email" onChange={handleChange} value={form.email} placeholder="Email"></input>
+            
+            <label>Resume:</label>
+            <input type="file" name="resume" onChange={handleFileChange} value={file} accept=".pdf,.doc,.docx" placeholder="Resume"></input>
             <button onClick={submitForm}>Send</button>
         </form>
         <h1>Form Test</h1>
