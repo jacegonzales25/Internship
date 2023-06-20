fd2e0f2e87e6cace9a1ec82cf8fa580b818f59e4
Jace Gonzales <jacegonzales25@gmail.com>
2023-06-20 12:32:33 +0800

    Added BISTRO File form

---
+++ bistro_email_sender/app/TGIF/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+import axios from 'axios';
+import React, { useState } from 'react';
+export default function TGIF() {
+    const formData = new FormData();
+
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        resume: ""
+    });
+
+    function submitForm(event: any) {
+        
+    }
+    
+
+    
+
+    return (
+      <div>
+        <form>
+            <label>TGIF Sender</label>
+            <input name="name" value={form.name} placeholder="Name"></input>
+            <input name="email" value={form.email} placeholder="Email"></input>
+            <button onClick={submitForm}>Send</button>
+        </form>
+        <h1>Form Test</h1>
+      </div>
+    
+    )
+  }
